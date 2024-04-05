2d0b8066b37e0093edf674e3d4e59742249b623b
Ben Calabrese <bencalabrese@gmail.com>
2024-04-05 16:51:23 -0700

    Update scripts after fresh install

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
-# Manual install list
-#
-# DisplayLink
-# Divvy
-# MuteDeck
-# Stream Deck
-# Control Center
-# LogiOptions+
-# HiddenBar
-# VS Code
-#   - Login to sync settings
+read -r -p "Check that the terminal app has 'Privacy and Security > App Management' permissions first. Press any key to continue..."
 
 # Install Homebrew itself
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Make sure we can also get fonts
+brew tap homebrew/cask-fonts
 
 # First install stow so we can get our various configs in place. We want these established before we
 # install applications that might write to them.
@@ -23,20 +17,27 @@ brew install stow
 stow --adopt symlinks
 git restore .
 
-
-# MesloLG Nerd Font
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.0/Meslo.zip \
-  && cd ~/.local/share/fonts \
-  && unzip Meslo.zip \
-  && rm Meslo.zip \
-  && fc-cache -fv
-
 # Finally, use Homebrew to install other utils
 brew install \
+  iterm2 \
   nvm \
-  starship
+  starship \
+  raycast \
+  elgato-stream-deck \
+  elgato-control-center \
+  logi-options-plus \
+  mutedeck \
+  visual-studio-code \
+  hiddenbar \
+  displaylink \
+  font-meslo-lg-nerd-font
 
-touch ~/.additional_machine_specific_zshrc
-source ~/.zshrc
+touch ~/.machine_specific_zshrc
+
+# This is a repeat of what's happening in our RC but we need to run it so that we can actually use
+# nvm below and sourcing the whole RC here has been problematic.
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 nvm install node
--- symlinks/.zshrc
+++ symlinks/.zshrc
@@ -1,13 +1,13 @@
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH="/usr/local/sbin:$PATH"
 
-eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
-[ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 eval "$(starship init zsh)"
 
-source ./.aliases.sh
-source ./.additional_machine_specific_zshrc
+source ~/.aliases.sh
+source ~/.machine_specific_zshrc
