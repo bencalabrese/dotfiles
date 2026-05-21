10d3dbca2823b7a3505118d910b6e5846a2149df
Ben Calabrese <ben.calabrese@vanta.com>
2026-05-21 10:48:18 -0700

    add auto mode alias

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 alias ..="cd .."
-
+alias acl="claude --permission-mode auto"
 # Linux GNU ls colorizes by default; match macOS behavior
 [[ "$DOTFILES_OS" == "Linux" ]] && alias ls='ls --color=never'
